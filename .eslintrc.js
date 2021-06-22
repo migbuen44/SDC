bbdbe8a65c9c912c2fa1c61cc2cd486b9685f7ed
Jeremy Lee <jeremysylee@gmail.com>
2021-06-21 14:12:59 -1000

    Reinstalled airBnbn linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,12 @@
-"extends": "airbnb"
-
-
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "commonjs": true,
-//         "es2021": true
-//     },
-//     "parserOptions": {
-//         "ecmaVersion": 12
-//     },
-//     "rules": {
-//     }
-// };
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+    }
+};
