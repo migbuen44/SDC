3db682db5badd009ef72bcb6b4e93a2aa2fe12e9
Jeremy Lee <jeremysylee@gmail.com>
2021-06-21 14:16:01 -1000

    Removed .js from eslintrc file to enable eslint. Airbnb linter now functional

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const router = require('./router.js');
+
 const port = 3001;
 const app = express();
 
