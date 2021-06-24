efe830fd6a327b825e0d63a8438599f05769b00a
Miguel Buenviaje <migbuen44@gmail.com>
2021-06-23 17:00:03 -0700

    Added lines to QANDA.sql file to copy users form temp questions file to users table with no duplicates

--- QANDA.sql
+++ QANDA.sql
@@ -5,7 +5,8 @@ CREATE DATABASE qanda;
 CREATE TABLE users (
   id serial PRIMARY KEY,
   name VARCHAR,
-  email VARCHAR
+  email VARCHAR,
+  UNIQUE (name, email)
 );
 
 CREATE TABLE questions (
@@ -46,3 +47,15 @@ CREATE TABLE questions_temp (
 );
 
 COPY questions_temp FROM '/Users/miguelbuenviaje/SDC/q_and_a_CSV/questions.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO users (name, email) SELECT DISTINCT name, email FROM questions_temp;
+
+-- INSERT INTO questions (product_id, body, date_written, reported, helpful, userId)
+
+-- id serial PRIMARY KEY,
+--   product_id INT,
+--   body VARCHAR,
+--   date_written BIGINT,
+--   userId INT REFERENCES users (id),
+--   reported INT,
+--   helpful INT
