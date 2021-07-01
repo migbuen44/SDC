f83d9e5f29688e2fc473b6a8a2c322eaeab34dd6
Jeremy Lee <jeremysylee@gmail.com>
2021-06-30 18:14:09 -1000

    test

--- server/router.js
+++ server/router.js
@@ -1,24 +1,15 @@
 const router = require('express').Router();
 const cors = require('cors');
 const controllers = require('./controllers');
-<<<<<<< HEAD
 const overview = require('./controllers/overview');
 
-=======
 const controller = require('./controllers/reviewsJG');
 
 
->>>>>>> b4e4c26c5f3e64dc1fe1492429c2be001188e87b
-// router.get('/o', controllers.overview.get)
-//   .get('/q', controllers.qAnda.get)
-//   .get('/r', controllers.reviews.get);
-
-<<<<<<< HEAD
 router.get('/products', overview.getProducts);
 router.get('/products/:productId', overview.getProductById);
 router.get('/products/:productId/styles', overview.getStyles);
 router.get('/products/:productId/related', overview.getRelated);
-=======
 
 router
 .route('/reviews/meta')
@@ -37,6 +28,5 @@ router
 router
   .route('/reviews/:id/report')
   .put(controller.updateReport);
->>>>>>> b4e4c26c5f3e64dc1fe1492429c2be001188e87b
 
 module.exports = router;
