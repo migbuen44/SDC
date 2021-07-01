777095d553899daa30ad80244c2b9a32f81402a9
Miguel Buenviaje <migbuen44@gmail.com>
2021-06-30 21:28:12 -0700

    Resolve merge conflicts

--- database/index.js
+++ database/index.js
@@ -1,24 +1,16 @@
-<<<<<<< HEAD
 // const { Pool } = require('pg');
 // const config = require('../config/config.js');
-=======
 const { Pool } = require('pg');
 const config = require('../config/config');
->>>>>>> 096ce07f431b4f4f0a329c55fed39141158d8eb0
 
-<<<<<<< HEAD
 // const pool = new Pool({ config });
-=======
 const pool = new Pool(config);
->>>>>>> b4e4c26c5f3e64dc1fe1492429c2be001188e87b
 
-<<<<<<< HEAD
 // pool.connect((err) => {
 //   err ? console.error(err) : console.log('Connected to database!');
 // });
 
 // module.exports = pool;
-=======
 pool.connect((err) => {
   if (err) { console.error(err); }
   // err ? console.error(err) : console.log('Connected to database!');
@@ -31,4 +23,3 @@ pool.connect((err) => {
 // });
 
 module.exports = pool;
->>>>>>> 096ce07f431b4f4f0a329c55fed39141158d8eb0
